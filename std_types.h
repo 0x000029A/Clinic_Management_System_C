3b0d70e240841e3e9a9273e82e3c031dc7671f4c
Mahmoud Hashem <89657916+0x000029A@users.noreply.github.com>
2022-12-31 17:53:24 +0200

    Create std_types.h

---
+++ std_types.h
@@ -0,0 +1,19 @@
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char u8;       // 1 byte  unsigned char     variable %c
+typedef signed char s8;         // 1 byte  signed   char     variable %c
+
+typedef unsigned short u16;     // 2 bytes unsigned integer  variable %hu
+typedef signed short s16;       // 2 bytes signed   integer  variable %hd
+
+typedef unsigned long u32;      // 4 bytes unsigned integer  variable %lu
+typedef signed long s32;        // 4 bytes signed   integer  variable %ld
+
+typedef unsigned long long u64; // 8 bytes unsigned integer  variable %llu
+typedef signed long long s64;   // 8 bytes signed   integer  variable %lld
+
+typedef float f32;              // 4 bytes signed   floating variable %f
+typedef double f64;             // 8 bytes signed   floating variable %lf
+
+#endif
